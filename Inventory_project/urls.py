8277e1d9dc5169f3d084af5c711c3821587041e6
Ndungu-Jn <johnnyostin6@gmail.com>
2024-12-07 17:15:10 +0300

    Added staff details and made orders functional.

--- Inventory_project/urls.py
+++ Inventory_project/urls.py
@@ -30,6 +30,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('dashboard', views.index, name='index'),
     path('staff', views.staff, name='staff'),
+    path('staff/datails/<int:pk>', views.staff_details, name='staff_details'),
     path('product', views.product, name='product'),
     path('product/delete/<int:pk>', views.product_delete, name='product_delete'),
     path('product/update/<int:pk>', views.product_update, name='product_update'),
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,9 @@ from lib2to3.fixes.fix_input import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Order
 from .forms import ProductForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -15,7 +16,18 @@ def index(request):
 
 @login_required
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    workers = User.objects.all()
+    context = {
+        'workers': workers
+    }
+    return render(request, 'dashboard/staff.html', context)
+@login_required
+def staff_details(request, pk):
+    workers = User.objects.get(id=pk)
+    context = {
+        'workers': workers,
+    }
+    return render(request, 'dashboard/staff_details.html', context)
 
 @login_required
 def product(request):#using ORM
@@ -36,15 +48,21 @@ def product(request):#using ORM
 
 @login_required
 def order(request):
-    return render(request, 'dashboard/order.html')
+    orders = Order.objects.all()
 
+    context = {
+        'orders': orders
+    }
+    return render(request, 'dashboard/order.html', context)
+
+@login_required
 def product_delete(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
         item.delete()
         return redirect("product")
     return render(request, 'dashboard/product_delete.html')
-
+@login_required
 def product_update(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -60,4 +78,7 @@ def product_update(request, pk):
     }
 
     return render(request, 'dashboard/product_update.html', context)
+
+
+
 
