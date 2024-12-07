4b136c3ff463e72a20ca18e00695dc2e64825cc4
Ndungu-Jn <johnnyostin6@gmail.com>
2024-12-07 16:11:23 +0300

    Encompassed the CRUD operation.

--- Inventory_project/urls.py
+++ Inventory_project/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('dashboard', views.index, name='index'),
     path('staff', views.staff, name='staff'),
     path('product', views.product, name='product'),
+    path('product/delete/<int:pk>', views.product_delete, name='product_delete'),
+    path('product/update/<int:pk>', views.product_update, name='product_update'),
     path('order', views.order, name='order'),
 
     path('admin/', admin.site.urls),
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -14,9 +18,46 @@ def staff(request):
     return render(request, 'dashboard/staff.html')
 
 @login_required
-def product(request):
-    return render(request, 'dashboard/product.html')
+def product(request):#using ORM
+    items = Product.objects.all()
+
+    if  request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(product)
+    else:
+        form = ProductForm()
+    context = {
+        'items': items,
+        'form': form,
+    }
+    return render(request, 'dashboard/product.html', context)
 
 @login_required
 def order(request):
     return render(request, 'dashboard/order.html')
+
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect("product")
+    return render(request, 'dashboard/product_delete.html')
+
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect("product")
+    else:
+        form = ProductForm(instance=item)
+    context={
+        'form': form,
+
+    }
+
+    return render(request, 'dashboard/product_update.html', context)
+
